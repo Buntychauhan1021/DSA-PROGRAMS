fd5bed2afa0390f0a08c674b2a29cce4f4bba278
Bunty chauhan <96004538+Buntychauhan1021@users.noreply.github.com>
2022-04-13 23:44:47 +0530

    update

---
+++ deleteAfterIndex.c
@@ -0,0 +1,19 @@
+# include<stdio.h>
+# include<stdlib.h>
+struct node {
+    int data;
+    struct node *next;
+
+};
+int main(){
+    struct node *head;
+    struct node *second;
+    struct node *three;
+    struct node *four;
+
+    head=(struct node *)malloc(sizeof(struct node));
+     second=(struct node *)malloc(sizeof(struct node));
+      three=(struct node *)malloc(sizeof(struct node));
+       four=(struct node *)malloc(sizeof(struct node));
+    
+    }
---
+++ inserAfterNode.c
@@ -0,0 +1,59 @@
+# include<stdio.h>
+# include<stdlib.h>
+struct node{
+    int data;
+    struct node * next;
+
+};
+struct node linkedlisttraversal( struct node * ptr)
+{
+    while (ptr!=NULL)
+    {
+        printf("Element : %d\n",ptr->data);
+        ptr=ptr->next; 
+    }
+}
+
+    struct node *insertAfterNode(struct node *head,struct node *previousnode,int data){
+
+         struct node *ptr =(struct node *)malloc(sizeof(struct node));
+         ptr ->data=data;
+         ptr->next=previousnode->next;
+         previousnode->next=ptr;
+
+        return head;
+
+    }
+
+
+
+int main(){
+   struct node * head;
+    struct node * second;
+     struct node * third;
+      struct node *four;
+head=(struct node *)malloc(sizeof(struct node*));
+second=(struct node *)malloc(sizeof(struct node*));
+third=(struct node *)malloc(sizeof(struct node*));
+four=(struct node *)malloc(sizeof(struct node*));
+head->data=7;
+head->next=second;
+
+second->data=5;
+second->next=third;
+
+third->data=9;
+third->next=four;
+
+four->data=11;
+four->next=NULL;
+printf("linked before delete\n");
+linkedlisttraversal(head);
+
+head=insertAfterNode(head,second,2);
+
+printf("linked after delete\n");
+linkedlisttraversal(head);
+
+     
+}
