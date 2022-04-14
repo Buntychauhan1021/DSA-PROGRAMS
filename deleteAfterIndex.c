9d66222faac7100d3adff407e3d7e28bbe3141cb
Bunty chauhan <96004538+Buntychauhan1021@users.noreply.github.com>
2022-04-14 23:48:26 +0530

    update

--- deleteAfterIndex.c
+++ deleteAfterIndex.c
@@ -5,6 +5,15 @@ struct node {
     struct node *next;
 
 };
+
+struct node traverse(struct node *ptr)
+{
+    while(ptr != NULL){
+    printf("Element : %d\n",ptr->data);
+    ptr=ptr->next;
+}
+}
+
 int main(){
     struct node *head;
     struct node *second;
@@ -15,5 +24,31 @@ int main(){
      second=(struct node *)malloc(sizeof(struct node));
       three=(struct node *)malloc(sizeof(struct node));
        four=(struct node *)malloc(sizeof(struct node));
-    
-    }
+
+    head->data=90;
+    head->next=second;
+
+    second->data=45;
+    second->next=three;
+
+    three->data=67;
+    three->next=four;
+
+    four->data=40;
+    four->next=NULL;
+//   int n;
+//     printf("enter the choice\n");
+//     scanf("%d",n);
+//    switch(n){
+//        case 1:
+       printf("traverse\n");
+       traverse(head);
+   //    break;
+
+    //    default:
+    //    printf("enter valid choice\n");
+    //    break;
+
+       
+   // }
+}
